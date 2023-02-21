3e38b31bfc5b62157be747e23d44d79d8757ce1b
Sunzheini <daniel_zorov@abv.bg>
2023-02-21 19:35:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+let a = 5
+let b = 8
+
+if (b > a) {
+    console.log(b)
+}
+
+
+// ---------------- //
+function solve(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber)
+}
+
+solve(5, 4)
+
+
+// ---------------- //
+function solve2(name, age) {
+    let output = `My name is ${name} and age is ${age}`;
+    console.log(output);
+}
+
+solve2('Daniel', 20);
+
+
+// ---------------- //
+function solve3(grade) {
+    console.log(grade.toFixed(2));
+}
+
+solve3(4.44242);    //4.44, returns a rounded string!
+
+
+// ---------------- //
+let isTrue = true;
+let empty = null;
+let unknown = undefined; //ne deklarirana promenliva
+
+let name = 3;
+var secondName = 4;
+
+
